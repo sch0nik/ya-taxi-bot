c6adc8d4451a7f27118097eb5a3a78362c576409
sch0nik <sch-nikolaj@yandex.ru>
2024-03-07 17:46:22 +0300

    reformat

--- main.py
+++ main.py
@@ -13,12 +13,17 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
 )
 
 load_dotenv()
+
+TOKEN = os.environ.get('TOKEN')
+if not TOKEN:
+    raise ValueError('Не удалось получить тг-токен')
 
 ChromeOptions = webdriver.ChromeOptions()
 ChromeOptions.add_argument('no-sandbox')
@@ -27,27 +32,20 @@ ChromeOptions.add_argument('-headless')
 ChromeOptions.add_argument('--enable-logging')
 ChromeOptions.add_argument('--v=1')
 ChromeOptions.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-
 ChromeService = Service(ChromeDriverManager().install())
 
-TOKEN = os.environ.get('TOKEN')
-if not TOKEN:
-    raise ValueError('Не удалось получить тг-токен')
-
 application = ApplicationBuilder().token(TOKEN).build()
-
-LIST_TAXI = []
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text='''Локальный бот для тестов. Сейчас яндекс-доставка.\
-         Отправьте мне или в чат сообщение в котором только ссылка на доставку'''
+        text=('''Локальный бот для тестов. Сейчас яндекс-доставка.'''
+              '''Отправьте мне или в чат сообщение в котором только ссылка на доставку''')
     )
 
 
-async def extract_from_dostavka(browser):
+async def extract_info(browser):
     logs = browser.get_log('performance')
     for log in logs:
         l = json.loads(log['message'])['message']
@@ -60,10 +58,10 @@ async def extract_from_dostavka(browser):
                 if 'summary' in body['body']:
                     body['body'] = json.loads(body['body'])
                     info = (
-                        body['body'].get('performer').get('name') + '\n' +
-                        body['body'].get('performer').get('vehicle_model') + '\n' +
-                        body['body'].get('performer').get('vehicle_number') + '\n' +
-                        body['body'].get('summary') + '\n'
+                            body['body'].get('performer').get('name') + '\n' +
+                            body['body'].get('performer').get('vehicle_model') + '\n' +
+                            body['body'].get('performer').get('vehicle_number') + '\n' +
+                            body['body'].get('summary') + '\n'
                     )
                     if 'Доставлено' in body['body']['summary']:
                         return True, info
@@ -76,105 +74,36 @@ async def extract_from_dostavka(browser):
     return False, False
 
 
-async def extract_from_go_yandex(browser):
-    page = browser.page_source
-    if 'Поездка завершена' in page:
-        return True, 'Поездка завершена'
-    if 'Доставлено' in page:
-        return True, 'Доставлено'
-
-    logs = browser.get_log('performance')
-    for log in logs[::-1]:
-        l = json.loads(log['message'])['message']
-        if l['method'] == 'Network.responseReceived':
-            logging.info('Есть метод')
-            try:
-                body = browser.execute_cdp_cmd(
-                    'Network.getResponseBody',
-                    {'requestId': l['params']['requestId']},
-                )
-                if 'dostavka' in browser.current_url:
-                    if 'summary' in body['body']:
-                        body = json.loads(body['body'])
-                        info = (
-                            body.get('performer').get('name') + '\n' +
-                            body.get('performer').get('vehicle_number') + '\n' +
-                            body.get('performer').get('vehicle_model') + '\n' +
-                            body.get('summary') + '\n'
-                        )
-                        # if 'Доставлено' in body['body']['summary']:
-                        #     return True, info
-                        # else:
-                        #     return False, info
-                        return False, info
-                if 'go.yandex' in browser.current_url:
-                    if 'distance_left' in body['body'] and 'time_left' in body['body']:
-                        body = json.loads(body['body'])
-                        info = (
-                            body.get('performer').get('name') + '\n' +
-                            body.get('performer').get('vehicle_number') + '\n' +
-                            body.get('performer').get('vehicle_model') + '\n' +
-                            body.get('summary') + '\n'
-                        )
-                        return False, info
-
-            except WebDriverException:
-                continue
-            except JSONDecodeError:
-                continue
-    return False, False
-
-
-async def processing(url: str, chat_id, message, bot):
+async def taxi(url: str, chat_id, message, bot):
     logging.info('такси запущено')
     browser = webdriver.Chrome(service=ChromeService, options=ChromeOptions)
     browser.get(url)
     await asyncio.sleep(5)
 
-    extract = None
-    if 'dostavka' in url:
-        extract = extract_from_dostavka
-    if 'go.yandex' in url:
-        extract =  extract_from_go_yandex
-
     while True:
-        state, info = await extract(browser)
-
+        state, info = await extract_info(browser)
         if not (state or info):
             break
-
-        if state:
-            logging.info(f'Такси завершено. Отправка результата в чат')
-            await message.reply_text(info)
-            break
-
-        logging.info(f'Такси в пути. Отправка результата в чат')
         await bot.send_message(chat_id, info)
-
+        if state:
+            message.reply_text(info)
+            break
         await asyncio.sleep(10 * 60)
         browser.refresh()
         await asyncio.sleep(5)
-
     browser.close()
 
 
 async def start_taxi(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.info(
-        f'{update.effective_chat.effective_name}::{update.effective_chat.id}::{update.message.text}'
-    )
     urls = re.findall(
         r'https?:\/\/dostavka\.yandex\.ru\/route\/\S+',
         update.message.text
     )
-    for url in urls:
-        await processing(url, update.effective_chat.id, update.message, context.bot)
-
-    urls = re.findall(
-        r'https?:\/\/go\.yandex\/route\/\S+',
-        update.message.text
+    logging.info(
+        f'{update.effective_chat.effective_name}::{update.effective_chat.id}::{update.message.text}'
     )
     for url in urls:
-        await processing(url, update.effective_chat.id, update.message, context.bot)
+        await taxi(url, update.effective_chat.id, update.message, context.bot)
 
 
 if __name__ == '__main__':
